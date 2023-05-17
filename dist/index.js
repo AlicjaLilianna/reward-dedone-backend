5b59876240d985bfccc1853f334f1bfe1e25d169
Devtato <mstarzycki@gmail.com>
2023-05-17 22:50:22 +0100

    add conviniance scripts

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { GraphQLError } from 'graphql';
@@ -58,7 +67,7 @@ const getUser = token => {
 };
 startStandaloneServer(server, {
     listen: { port: APP_PORT },
-    context: async ({ req }) => {
+    context: ({ req }) => __awaiter(void 0, void 0, void 0, function* () {
         const token = req.headers.authorization || '';
         const user = getUser(token);
         if (DEPLOYMENT === 'development') {
@@ -73,6 +82,6 @@ startStandaloneServer(server, {
                 },
             });
         return { user };
-    }
+    })
 });
 //# sourceMappingURL=index.js.map
